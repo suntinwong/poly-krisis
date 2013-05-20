686956d411c4a8e574667c22c3588c27fd68641b
arellanj <jarel002@ucr.edu>
2013-05-19 19:37:24 -0700

    started the collision manager

---
+++ poly-krisis/poly-krisis/Classes/ColMan.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace poly_krisis.Classes
+{
+    
+    class AABB
+    {
+        public Vector3 min, max;
+        AABB(Vector3 min, Vector3 max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    enum colobjectType{Player, Enemy, Static, PlayerAttack, EnemyAttack}
+    
+    class collisionObject
+    {
+        AABB boundingBox;
+        colobjectType type;
+
+        Int32 collisionMask;
+        collisionObject(AABB box, colobjectType collisionType)
+        {
+            boundingBox = box;
+            type = collisionType;
+        }
+
+        public void collide(collisionObject object2)
+        {
+            return;
+        }
+    }
+
+    class ColMan
+    {
+
+        List<collisionObject> objects;
+
+        ColMan()
+        {
+            objects = new List<collisionObject>();
+        }
+
+
+        void addObject(collisionObject obj)
+        {
+            objects.Add(obj);
+        }
+
+        void Update(GameTime time)
+        {
+            for (int i = 0 ; i < objects.Count ; i++ )
+            {
+                for ( int j = i+1 ; j < objects.Count ; j++)
+                {
+
+                    objects[i].collide( objects[j] );
+                }
+            }
+        }
+    }
+}
